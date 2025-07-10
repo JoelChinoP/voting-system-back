aa269f2b8b30cec5a22f42257732ad1930c06273
alexisBltz <almamanima@unsa.edu.pe>
2025-07-10 13:17:28 -0500

    feat: Enable JPA and Cassandra repositories in VotesServiceApplication

--- apps/votes-service/src/main/java/com/votes/VotesServiceApplication.java
+++ apps/votes-service/src/main/java/com/votes/VotesServiceApplication.java
@@ -2,8 +2,12 @@ package com.votes;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.votes.repository.postgres")
+@EnableCassandraRepositories(basePackages = "com.votes.repository.cassandra")
 public class VotesServiceApplication {
 
 	public static void main(String[] args) {
